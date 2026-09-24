e950d1d1683b1b4a99244d7245da9177c0bbfafd
Edward Monteiro <edward.monteiro@gmail.com>
2026-09-24 16:55:35 -0300

    Bump Aimotion to 0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.edwardresearchlabs.aimotion"
         minSdk = 29
         targetSdk = 37
-        versionCode = 8
-        versionName = "0.5.2"
+        versionCode = 9
+        versionName = "0.6.0"
     }
 
     compileOptions {
